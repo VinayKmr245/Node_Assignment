c21fe4b3df9b5cd2d26fcb4ec195f14a32057e9b
Vinay Kumar Mankala <vinay.m@comakeit.com>
2022-11-14 17:32:56 +0530

    2ndtime

---
+++ frontend/src/component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const component = () => {
+  return (
+    <div>
+        component
+        <h1>Testing merfe conflice</h1>
+    </div>
+  )
+}
+
+export default component
